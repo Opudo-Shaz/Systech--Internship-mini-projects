d4cd2b93e818ee9ad260bd08ea8990d5e22ab9f5
Opudo-Shaz <opudosharon6@gmail.com>
2026-01-16 14:28:11 +0300

    Add Main class with methods for prime number printing, menu display, and number guessing game

--- newprojectjava/src/main/java/com/example/calculator/Calculator.java
+++ newprojectjava/src/main/java/com/example/calculator/Calculator.java
@@ -1,4 +1,4 @@
-package com.example.calculator;
+package com.example.calculator; 
 
 public class Calculator {
 
@@ -7,6 +7,8 @@ public class Calculator {
         Calculator calc = new Calculator(10, 5, '+');
         double result = calc.calculate();
         System.out.println("Result: " + result);
+        System.out.println(calc.message());
+        calc.testIncrementOperators();
     }
     // program that takes two numbers and does math operations depending with operator user puts
     int a;
@@ -36,6 +38,17 @@ public class Calculator {
         }
     }
 
+    public String message(){
+        String status = "Calculator is operational.";
+        return status;
+    }
 
-    
+    public void testIncrementOperators(){
+        int x = 100; 
+        int a = x++;
+        int b = ++x;
+        int c = x++;
+        int d = (a<b)? (a<c)? a: (b<c)? b:c:x;
+        System.out.println(d);
+    }
 }
---
+++ newprojectjava/src/main/java/com/example/loops/Main.java
@@ -0,0 +1,117 @@
+package com.example.loops;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in); // one scanner for entire program
+
+    public static void main(String[] args) {
+ 
+        //printLoveYou100Times();
+        //showMenu();
+        printPrimeNumbers();
+        //breakingStatement();
+        //numberGuessingGame();
+
+    }
+ 
+    // for loop to print i love you 100 times
+    static void printLoveYou100Times() {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i + ": I love you");
+        }
+    }
+
+    // while loop to show menu until user exits
+    static void showMenu() {
+        int choice = 0;
+
+        while (choice != 4) {
+            System.out.println("\nMenu:");
+            System.out.println("1. TEA");
+            System.out.println("2. MANDAZI");
+            System.out.println("3. CHAPATI");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = scanner.nextInt();
+        }
+
+        System.out.println("Exiting the menu. Goodbye!");
+    }
+
+    // while loop with counter
+    public static void whileLoopWithCounter() {
+        int counter = 1;
+        while (counter <= 5) {
+            System.out.println("Counter: " + counter);
+            counter++;
+        }
+    }
+
+    // do while loop to play number guessing game
+    public static void numberGuessingGame() {
+        int secretNumber = 7;
+        int guess;
+
+        do {
+            System.out.print("Guess the number (1-10): ");
+            guess = scanner.nextInt();
+
+            if (guess < secretNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > secretNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the number.");
+            }
+
+        } while (guess != secretNumber);
+    }
+
+    // break and continue example
+    public static void breakingStatement() {
+
+        int[] numbers = {11, 23, 39, 47, 58, 69};
+
+        for (int number : numbers) {
+            if (number == 47) {
+                System.out.println("Number 47 found! Exiting loop.");
+                break;
+            }
+        }
+
+        System.out.println("Odd Numbers:");
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+
+    // print prime numbers between 1 and 50
+    public static void printPrimeNumbers() {
+
+        for (int i = 1; i <= 50; i++) {
+
+            if (i < 2) {
+                continue;
+            }
+
+            boolean isPrime = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
+    }
+}
