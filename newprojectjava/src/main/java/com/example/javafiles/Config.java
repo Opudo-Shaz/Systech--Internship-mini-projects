fec143fc793d0452bd04ac65a7ddde15c5a8543f
Opudo-Shaz <opudosharon6@gmail.com>
2026-02-24 10:46:47 +0300

    Add configuration and main classes for database connection

---
+++ newprojectjava/src/main/java/com/example/javafiles/Config.java
@@ -0,0 +1,9 @@
+package com.example.javafiles;
+
+public class Config {
+    private String username;
+    private String password;
+    private String url;
+    private String driver;  
+    
+}
---
+++ newprojectjava/src/main/java/com/example/javafiles/Main.java
@@ -0,0 +1,9 @@
+package com.example.javafiles;
+
+public class Main {
+
+    public static void main(String[] args) {
+     //file operations and file utility 
+       
+    }
+}
