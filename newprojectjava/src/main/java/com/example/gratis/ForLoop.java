637596dcced34dffaff03c8788d613e4239f8799
Opudo-Shaz <opudosharon6@gmail.com>
2026-06-16 13:29:15 +0300

    xyx

---
+++ newprojectjava/src/main/java/com/example/gratis/ForLoop.java
@@ -0,0 +1,11 @@
+package com.example.gratis;
+public class ForLoop {
+    
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int e = 0; e < 5; e += 2) {
+            System.out.println(numbers[e]);
+        }
+    }
+    
+}
